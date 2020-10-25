d294e1ee7eb5a821559dbb3c06ee9b4e8289d13b
Kashtanow Anton <kashtanow.anton@gmail.com>
2020-10-25 16:35:04 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { SocratesModule } from './modules/socrates/socrates.module';
+import { EsperantoModule } from './modules/esperanto/esperanto.module';
+import { EnglishModule } from './modules/english/english.module';
+import { BlogModule } from './modules/blog/blog.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BlogModule,
+    EnglishModule,
+    EsperantoModule,
+    SocratesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
