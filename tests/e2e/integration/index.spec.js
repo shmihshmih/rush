04b8dfe30fe65b17f81eb8923f9e5e6f32b19837
Anton <shmihshmih@gmail.com>
2021-10-31 22:44:13 +0300

    cypress start

---
+++ tests/e2e/integration/index.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+describe('index page', () => {
+  it('должно показываться название страницы или что там показывается', () => {
+    cy.visit('/');
+
+    cy.get('.header-logo > a')
+      .should('contain.text', 'Логово Голема')
+
+    cy.get(':nth-child(1) > .mat-card-actions > .mat-focus-indicator').click();
+
+    cy.get('button.mat-focus-indicator.mat-button.mat-button-base.mat-primary > span')
+      .should('contain.text', 'Ответ');
+  })
+})
