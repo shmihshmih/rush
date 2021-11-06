ea8dab2923ba67b900b5a8e1a1f84d25acfb4246
Anton <shmihshmih@gmail.com>
2021-11-06 21:37:21 +0300

    sdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
     const words = collection(firestore, 'words');
     this.item$ = collectionData(words);
 
-    this.item$.subscribe(w => console.log('ws!Nbgf ds[jlyq yj yflj rvvbn?: ', w));
+    this.item$.subscribe(w => console.log('ws!Nbgf ds[jlyq yj yflj rvhhnjkvbn?: ', w));
 
     /** Загрузка данных для модуля с языками */
     // получение списков слов диспатч
