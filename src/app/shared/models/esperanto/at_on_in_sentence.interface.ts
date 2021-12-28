d6f3b296bad6de48f32491165d411abe2a6f86ff
Anton <shmihshmih@gmail.com>
2021-12-28 19:22:25 +0300

    дополнил данные по at on in

--- src/app/shared/models/esperanto/at_on_in_sentence.interface.ts
+++ src/app/shared/models/esperanto/at_on_in_sentence.interface.ts
@@ -2,4 +2,7 @@ export interface IPrepositionExercise {
   id: string;
   preposition: 'at' | 'on' | 'in'; // at on in
   word: string; // monday
+  russian: string; // перевод
+  type: 'simple' | 'phrase'; // обычный пример или устойчивое выражение
+  example?: string[];
 }
